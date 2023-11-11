c806029b3e237d2c0c0941f56ea0e78ad82831c4
Tomas Protivinsky <tomas.protivinsky@gmail.com>
2023-11-11 19:47:56 +0100

    admission data and allocation in graphic logger

--- admissions/logger/full_loggers.py
+++ admissions/logger/full_loggers.py
@@ -22,24 +22,69 @@ class GraphicLogger(Logger):
 
     def log_start(self, admission_data: AdmissionData):
         doc = self.doc
+
         doc.line(self._header, "Vstupní data")
 
+        doc.line(self._subheader, "Studentské přihlášky")
         with doc.tag("table", klass="admission-table"):
-            with doc.tag("tr"):
-                doc.line("th", "Studentské přihlášky", colspan=4)
             with doc.tag("tr"):
                 for t in ["", "1. škola", "2. škola", "3. škola"]:
                     doc.line("th", t)
             for st, schs in admission_data.applications.items():
                 with doc.tag("tr"):
-                    with doc.tag("th"):
+                    with doc.tag("th", klass="app-student"):
                         doc.line("i", "", klass="bi bi-person-fill")
                         doc.text(f"  {st}")
                     for sch in schs:
                         doc.line("td", str(sch))
 
+        schools = list(admission_data.exams.keys())
+        max_exam_len = max([len(ex) for ex in admission_data.exams.values()])
+
+        doc.line(self._subheader, "Výsledky školních zkoušek")
+        with doc.tag("table", klass="admission-table"):
+            with doc.tag("tr"):
+                doc.line("th", "")
+                for sch in schools:
+                    with doc.tag("th", klass="exam-school"):
+                        doc.line("i", "", klass="bi bi-house-fill")
+                        doc.text(f"  {sch}")
+            for i in range(max_exam_len):
+                with doc.tag("tr"):
+                    doc.line("td", f"{i + 1}.")
+                    for sch in schools:
+                        st = admission_data.exams[sch][i]
+                        with doc.tag("td", klass="exam-student"):
+                            doc.line("i", "", klass="bi bi-person-fill")
+                            doc.text(f"  {st}")
+
     def log_step(self, data: Mapping):
         ...
 
     def log_end(self, allocation: Allocation):
-        ...
+        doc = self.doc
+        doc.line(self._header, "Výsledek přijímaček")
+
+        schools = sorted(list(allocation.accepted.keys()))
+
+        # accepted
+        doc.line(self._subheader, "Přijatí žáci")
+        with doc.tag("ul", klass="admission-allocation"):
+            for sch in schools:
+                sts = allocation.accepted[sch]
+                with doc.tag("li"):
+                    doc.line("i", "", klass="bi bi-house-fill")
+                    doc.line("b", f"  {sch}")
+                    doc.line("i", "", klass="bi bi-arrow-left")
+                    doc.line("i", "", klass="bi bi-person-fill")
+                    st_text = "( " + ", ".join((f"  {st}" for st in sts)) + " )"
+                    doc.text(st_text)
+
+        # rejected
+        if allocation.rejected:
+            doc.line(self._subheader, "Nepřijatí žáci")
+            with doc.tag("ul", klass="admission-allocation"):
+                for st in allocation.rejected:
+                    with doc.tag("li"):
+                        doc.line("i", "", klass="bi bi-person-fill")
+                        doc.text(str(st))
